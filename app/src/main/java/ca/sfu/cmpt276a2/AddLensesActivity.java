e9e60a76757396360ec40b55d95a88a89d3d3b7d
Sarbjot <sarbjotmann98@gmail.com>
2020-06-09 17:51:25 -0700

    [sarbjotm] - Created a check for when the user does not enter an element for adding a news lens, and gives the user an error message, this makes the app not crash if a empty string is entered

--- app/src/main/java/ca/sfu/cmpt276a2/AddLensesActivity.java
+++ app/src/main/java/ca/sfu/cmpt276a2/AddLensesActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
 public class AddLensesActivity extends AppCompatActivity {
 
     private String make;
@@ -44,19 +45,41 @@ public class AddLensesActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent();
                 EditText editMake = (EditText) findViewById(R.id.editTextCameraMake);
-                String make = editMake.getText().toString();
-                intent.putExtra("modelMake", make);
+                String make = editMake.getText().toString().trim();
 
                 EditText editFocalLength = (EditText) findViewById(R.id.editFocalLength);
-                String focalLength = editFocalLength.getText().toString();
-                intent.putExtra("modelFocal", focalLength);
+                String focalLength = editFocalLength.getText().toString().trim();
 
                 EditText editAperture = (EditText) findViewById(R.id.editAperture);
-                String apertureLength = editAperture.getText().toString();
+                String apertureLength = editAperture.getText().toString().trim();
+
+                if (make.isEmpty()){
+                    editMake.setError("Field can't be empty");
+                }
+
+                else if (focalLength.isEmpty()){
+                    editFocalLength.setError("Field can't be empty");
+                }
+
+                else if (apertureLength.isEmpty()){
+                    editAperture.setError("Field can't be empty");
+                }
+
+                else{
+
+
+
+                intent.putExtra("modelMake", make);
+
+
+                intent.putExtra("modelFocal", focalLength);
+
+
                 intent.putExtra("modelAperture", apertureLength);
 
                 setResult(Activity.RESULT_OK, intent);
                 finish();
+                }
             }
 
         });
