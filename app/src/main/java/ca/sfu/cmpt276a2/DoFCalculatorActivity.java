edf28d09cac8fdc999df1c672051d0145c991635
Sarbjot <sarbjotmann98@gmail.com>
2020-06-09 18:09:41 -0700

    [sarbjotm] - Empty String Error Handling for DoF

--- app/src/main/java/ca/sfu/cmpt276a2/DoFCalculatorActivity.java
+++ app/src/main/java/ca/sfu/cmpt276a2/DoFCalculatorActivity.java
@@ -2,9 +2,13 @@ package ca.sfu.cmpt276a2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import ca.sfu.cmpt276a2.Model.DepthOfFieldCalculator;
 import ca.sfu.cmpt276a2.Model.Lens;
@@ -18,7 +22,49 @@ public class DoFCalculatorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_do_f_calculator);
         this.setTitle("Calculate DoF");
+        calculateButtonFunction();
+
     }
+
+    private void calculateButtonFunction(){
+        Button btn = (Button) findViewById(R.id.calculateButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                EditText editCircleofConfusion = (EditText) findViewById(R.id.editcircleOfConfusion);
+                String circleOfConfusion = editCircleofConfusion.getText().toString().trim();
+
+                EditText editDistance = (EditText) findViewById(R.id.editDistance);
+                String distance = editDistance.getText().toString().trim();
+
+                EditText editApertureInDoF = (EditText) findViewById(R.id.editApertureInDoF);
+                String aperture = editApertureInDoF.getText().toString().trim();
+
+                if (circleOfConfusion.isEmpty()){
+                    editCircleofConfusion.setError("Field can't be empty");
+                }
+
+                else if (distance.isEmpty()){
+                    editDistance.setError("Field can't be empty");
+                }
+
+                else if (aperture.isEmpty()){
+                    editApertureInDoF.setError("Field can't be empty");
+                }
+
+                else{
+                    intent.putExtra("calculateCircleOfConfusion", circleOfConfusion);
+                    intent.putExtra("calculateDistance", distance);
+                    intent.putExtra("Aperture", aperture);
+                    finish();
+                }
+            }
+
+        });
+    }
+
+
 
     public static Intent makeIntent(Context context){
         return new Intent(context, DoFCalculatorActivity.class);
