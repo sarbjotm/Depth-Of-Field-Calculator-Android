ea34be74d4e9d68e19618850abd0f97053ae3baf
Sarbjot <sarbjotmann98@gmail.com>
2020-06-12 23:59:49 -0700

    small bug

--- app/src/main/java/ca/sfu/cmpt276a2/Model/Lens.java
+++ app/src/main/java/ca/sfu/cmpt276a2/Model/Lens.java
@@ -44,6 +44,6 @@ public class Lens {
 
     @Override
     public String toString() {
-        return make + ' ' + maxAperture + "mm F" + focalLength;
+        return make + ' ' + focalLength + "mm F" + maxApertureh;
     }
 }
